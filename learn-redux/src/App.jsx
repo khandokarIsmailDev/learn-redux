72af01f43a9c545658e006479daa104017caf332
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-01-05 19:29:06 +0600

    firsttime data get from redux

--- learn-redux/src/App.jsx
+++ learn-redux/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Coutner from './component/Coutner'
+
 
 export default function App() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline text-blue-500' >Hello World</h1>
+      <Coutner/>
     </div>
   )
 }
--- learn-redux/src/component/Coutner.jsx
+++ learn-redux/src/component/Coutner.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
-export default function Coutner({count, handleIncrement, handleDecrement, id}) {
-    
+export default function Coutner() {
+    const counters = useSelector(state => state.counters)
+    console.log(counters)
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button className='bg-blue-500 text-white px-4 py-2 rounded-md' onClick={() => handleIncrement(id)}>Increment</button>
-      <button className='bg-blue-500 text-white px-4 py-2 rounded-md' onClick={handleDecrement}>Decrement</button>
+      <h1>Count: {counters[0].value}</h1>
+      <h1>Count: {counters[1].value}</h1>
+      
     </div>
   )
 }
