1703a956d49ded0ebd7545a84728110d07af4f00
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-01-05 21:34:56 +0600

    useSelector & useDispatch learn and apply

--- learn-redux/src/component/Coutner.jsx
+++ learn-redux/src/component/Coutner.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement } from '../freatures/counters/counterSlice'
 
 export default function Coutner() {
     const counters = useSelector(state => state.counters)
     console.log(counters)
+    const dispatch = useDispatch()
+
+    const handleIncrement = (id) => {
+        dispatch(increment(id))
+    }
+    const handleDecrement = (id) => {
+        dispatch(decrement(id))
+    }
 
   return (
     <div>
-      <h1>Count: {counters[0].value}</h1>
-      <h1>Count: {counters[1].value}</h1>
-      
+      {counters.map((counter) => (
+        <div key={counter.id}>
+          <h1>Count: {counter.value}</h1>
+          <button onClick={() => handleIncrement(counter.id)}>Increment</button>
+          <button onClick={() => handleDecrement(counter.id)}>Decrement</button>
+        </div> 
+      ))}
     </div>
   )
 }
